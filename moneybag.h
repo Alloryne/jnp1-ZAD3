2c4b5db29362efd056f424f817bb4194caa4f45f
Łukasz Łopacki <ll439936@students.mimuw.edu.pl>
2022-11-19 18:51:24 +0100

    wszystko w headerze

--- moneybag.h
+++ moneybag.h
@@ -20,35 +20,111 @@ private:
     coin_number_t m_denier;
 public:
     // Konstruktory
-    constexpr Moneybag( coin_number_t l, 
-                        coin_number_t s , 
-                        coin_number_t d);
+    constexpr Moneybag( coin_number_t l,
+                        coin_number_t s,
+                        coin_number_t d)
+            : m_livre(l), m_solidus(s), m_denier(d) {};
     constexpr Moneybag(const Moneybag&) = default;
-    Moneybag() = delete; 
+    Moneybag() = delete;
+
     // Akcesory
-    constexpr coin_number_t livre_number() const;
-    constexpr coin_number_t solidus_number() const;
-    constexpr coin_number_t denier_number() const;
+    constexpr coin_number_t livre_number() const{
+        return m_livre;
+    }
+    constexpr coin_number_t solidus_number() const{
+        return m_solidus;
+    }
+    constexpr coin_number_t denier_number() const{
+        return m_denier;
+    }
     // Operacje arytmetyczne z przypisaniem
-    Moneybag& operator+=(const Moneybag& rhs);
-    Moneybag& operator-=(const Moneybag& rhs);
-    Moneybag& operator*=(coin_number_t);
+    Moneybag& operator+=(const Moneybag& rhs){
+        if (m_livre + rhs.m_livre < m_livre ||
+            m_solidus + rhs.m_solidus < m_solidus ||
+            m_denier + rhs.m_denier < m_denier) {
+            throw std::out_of_range("Wynik z poza zakresu");
+        }
+        m_livre += rhs.m_livre;
+        m_solidus += rhs.m_solidus;
+        m_denier += rhs.m_denier;
+        return *this;
+    }
+    Moneybag& operator-=(const Moneybag& rhs){
+        if (m_livre - rhs.m_livre > m_livre ||
+            m_solidus - rhs.m_solidus > m_solidus ||
+            m_denier - rhs.m_denier > m_denier) {
+            throw std::out_of_range("Wynik z poza zakresu");
+        }
+        m_livre -= rhs.m_livre;
+        m_solidus -= rhs.m_solidus;
+        m_denier -= rhs.m_denier;
+        return *this;
+    }
+    Moneybag& operator*=(coin_number_t t){
+        if ((m_livre * t) / t != m_livre ||
+            (m_solidus * t) / t != m_solidus ||
+            (m_denier * t) / t != m_denier) {
+            throw std::out_of_range("Wynik z poza zakresu");
+        }
+        m_livre *= t;
+        m_solidus *= t;
+        m_denier *= t;
+        return *this;
+    }
     // Przypisywanie
     constexpr Moneybag& operator=(const Moneybag& rhs) = default;
     // Porównywanie
-    std::partial_ordering operator<=>(const Moneybag&) const;
+    std::partial_ordering operator<=>(const Moneybag& rhs) const{
+        if (m_livre == rhs.m_livre &&
+            m_solidus == rhs.m_solidus &&
+            m_denier == rhs.m_denier) {
+            return std::partial_ordering::equivalent;
+        }
+        if (m_livre <= rhs.m_livre &&
+            m_solidus <= rhs.m_solidus &&
+            m_denier <= rhs.m_denier) {
+            return std::partial_ordering::less;
+        }
+        if (m_livre >= rhs.m_livre &&
+            m_solidus >= rhs.m_solidus &&
+            m_denier >= rhs.m_denier) {
+            return std::partial_ordering::greater;
+        }
+        return std::partial_ordering::unordered;
+    }
     bool operator==(const Moneybag& rhs) const = default;
+
     // Rzutowanie na bool
-    constexpr explicit operator bool() const;
+     explicit operator bool() const{
+        return m_livre != 0 || m_solidus != 0 || m_denier != 0;
+     }
 };
 
 // Operacje arytmetyczne implementowane za operacji z klasy
-Moneybag operator+(const Moneybag& lhs, const Moneybag& rhs) ;
-Moneybag operator-(const Moneybag& lhs, const Moneybag& rhs);
-Moneybag operator*(const Moneybag::coin_number_t scalar, const Moneybag& rhs);
-Moneybag operator*(const Moneybag& lhs, const Moneybag::coin_number_t scalar);
+Moneybag operator+(const Moneybag& lhs, const Moneybag& rhs) {
+    return Moneybag(lhs) += rhs;
+}
+Moneybag operator-(const Moneybag& lhs, const Moneybag& rhs){
+    return Moneybag(lhs) -= rhs;
+}
+Moneybag operator*(const Moneybag::coin_number_t scalar, const Moneybag& rhs){
+    return Moneybag(rhs) *= scalar;
+}
+Moneybag operator*(const Moneybag& lhs, const Moneybag::coin_number_t scalar){
+    return Moneybag(lhs) *= scalar;
+}
 
-std::ostream& operator<<(std::ostream&, Moneybag const& );
+std::ostream& operator<<(std::ostream& os, Moneybag const& m){
+    os << "("
+       << m.livre_number()
+       <<(m.livre_number() == 1 ? " livr, " : " livres, ")
+       << m.solidus_number()
+       <<(m.solidus_number() == 1 ? " solidus, " : " soliduses, ")
+       << m.denier_number()
+       <<(m.denier_number() == 1 ? " denier" : " deniers")
+       << ")";
+    return os;
+}
 
 // Stałe obiekty inicjalizowane podczas kompilacji
 constinit const Moneybag Livre = Moneybag(1, 0, 0);
@@ -58,24 +134,34 @@ constinit const Moneybag Denier = Moneybag(0, 0, 1);
 class Value{
 public:
     // Konstruktory
-    explicit Value(Moneybag);
-    explicit Value(__uint128_t);
-    explicit Value();
+    explicit Value(Moneybag m)
+            : v(m.livre_number() * 240 + m.solidus_number() * 12 + m.denier_number()){}
+    explicit Value(__uint128_t n) : v(n) {}
+    explicit Value() : v(0){}
+
+
+
 
     // Kopiowanie
-    constexpr Value &operator=(const Value &) = default;
+    Value &operator=(const Value &) = default;
 
-    // Porównywanie obiektów Value, a także 
-    // obiektu Value z liczbą całkowitą nieujemną
-    std::strong_ordering operator<=>(Value const &) const = default;
-    constexpr bool operator==(Value const &) const = default;
-    std::strong_ordering operator<=>(const __uint128_t &) const;
-    constexpr bool operator==(const __uint128_t &) const;
     // Wypisywanie wartości
-    explicit operator std::string () const;
+    explicit operator std::string () const{
+        __uint128_t temp = v;
+
+        std::string str;
+        do {
+            uint8_t digit = temp % 10;
+            str = std::to_string(digit) + str;
+            temp = (temp - digit) / 10;
+        } while (temp != 0);
+        return str;
+    }
     // Rzut na int
-    Value::operator __uint128_t () const;
-}
+    operator __uint128_t () const{
+        return v;
+    }
+
 
 private:
     __uint128_t v;
